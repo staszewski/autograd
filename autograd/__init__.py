f613b5740071893da58f5864e4f0123089a021e4
staszewski <kamilstaszewski95@gmail.com>
2025-09-08 08:30:20 +0200

    feat: pow op

--- autograd/__init__.py
+++ autograd/__init__.py
@@ -7,7 +7,7 @@ A simple implementation of automatic differentiation for learning purposes.
 from .tensor import Tensor
 from .context import Context
 from .operation import Operation
-from .arithmetic import AddOperation, SubOperation, MulOperation
+from .arithmetic import AddOperation, SubOperation, MulOperation, PowOperation
 
 __version__ = "0.1.0"
-__all__ = ["Tensor", "Context", "Operation", "AddOperation", "SubOperation", "MulOperation"]
+__all__ = ["Tensor", "Context", "Operation", "AddOperation", "SubOperation", "MulOperation", "PowOperation"]
--- autograd/arithmetic.py
+++ autograd/arithmetic.py
@@ -45,3 +45,24 @@ class MulOperation(Operation):
         a, b = ctx.saved_tensors
 
         return [grad_output * b.data, grad_output * a.data]
+
+class PowOperation(Operation):
+    """Power operation."""
+    
+    @classmethod
+    def forward(cls, ctx: Context, a: 'Tensor', n: 'Tensor') -> np.ndarray:
+        ctx.save_for_backward(a, n)
+        return a.data ** n.data
+    
+    @classmethod
+    def backward(cls, ctx: Context, grad_output: np.ndarray) -> List[np.ndarray]:
+        a, n = ctx.saved_tensors
+
+        # d/da(a^n) = n * a^(n-1) -> Power Rule
+        grad_a = grad_output * n.data * (a.data ** (n.data - 1))
+        
+        # d/dn(a^n) = a^n * ln(a) -> Exponential Rule
+        import math
+        grad_n = grad_output * (a.data ** n.data) * math.log(a.data)
+
+        return [grad_a, grad_n]
