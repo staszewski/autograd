6337121e86677e0e40115af4cd4fec634cf53aca
staszewski <kamilstaszewski95@gmail.com>
2025-09-19 16:39:55 +0200

    feat: drone physics + nn

--- autograd/__init__.py
+++ autograd/__init__.py
@@ -10,6 +10,7 @@ from .operation import Operation
 from .arithmetic import AddOperation, SubOperation, MulOperation, PowOperation, DivOperation, ReLUOperation, MatMulOperation, SigmoidOperation, TanhOperation
 from .mlp import MLP
 from .loss import MSELoss, CrossEntropyLoss
+from .drone_problems.drone_physics import Drone
 
 __version__ = "0.1.0"
-__all__ = ["Tensor", "Context", "Operation", "AddOperation", "SubOperation", "MulOperation", "PowOperation", "DivOperation", "ReLUOperation", "MatMulOperation", "MLP", "MSELoss", "SigmoidOperation", "CrossEntropyLoss", "TanhOperation"]
+__all__ = ["Tensor", "Context", "Operation", "AddOperation", "SubOperation", "MulOperation", "PowOperation", "DivOperation", "ReLUOperation", "MatMulOperation", "MLP", "MSELoss", "SigmoidOperation", "CrossEntropyLoss", "TanhOperation", "Drone"]
--- autograd/mlp.py
+++ autograd/mlp.py
@@ -2,7 +2,7 @@ import numpy as np
 from autograd import Tensor
 
 class MLP:
-    def __init__(self, input_size, hidden_size, output_size, activation):
+    def __init__(self, input_size, hidden_size, output_size, activation = "relu"):
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
